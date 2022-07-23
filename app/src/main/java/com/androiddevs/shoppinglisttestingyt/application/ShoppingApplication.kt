025bbbb1371166cd48d2969989c0f32b8e086fb5
RyanKoech <sirryankoech@gmail.com>
2022-07-23 08:15:52 +0300

    Created and added application class for HILT-Dagger

---
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/application/ShoppingApplication.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.shoppinglisttestingyt.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShoppingApplication : Application()

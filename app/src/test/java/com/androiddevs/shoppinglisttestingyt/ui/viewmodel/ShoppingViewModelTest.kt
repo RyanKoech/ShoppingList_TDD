24afe730cb038a11505d8cef2f7fae7d5999926f
RyanKoech <sirryankoech@gmail.com>
2022-07-28 11:15:33 +0300

    Added Viewmodel test file

---
+++ app/src/test/java/com/androiddevs/shoppinglisttestingyt/ui/viewmodel/ShoppingViewModelTest.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.shoppinglisttestingyt.ui.viewmodel
+
+import com.androiddevs.shoppinglisttestingyt.repository.FakeShoppingRepository
+import org.junit.Assert.*
+import org.junit.Before
+
+class ShoppingViewModelTest

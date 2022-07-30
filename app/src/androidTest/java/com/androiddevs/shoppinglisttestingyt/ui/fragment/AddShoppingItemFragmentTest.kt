9166fbaea8702a392fd718e9bd94995e84b2bcdc
RyanKoech <sirryankoech@gmail.com>
2022-07-30 09:56:53 +0300

    Implemented test to assert that shopping item is inserted into db

--- app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ui/fragment/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ui/fragment/AddShoppingItemFragmentTest.kt
@@ -1,18 +1,20 @@
 package com.androiddevs.shoppinglisttestingyt.ui.fragment
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.androiddevs.shoppinglisttestingyt.R
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
 import com.androiddevs.shoppinglisttestingyt.getOrAwaitValue
 import com.androiddevs.shoppinglisttestingyt.launchFragmentInHiltContainer
 import com.androiddevs.shoppinglisttestingyt.repository.FakeShoppingRepositoryAndroidTest
+import com.androiddevs.shoppinglisttestingyt.ui.fragment.factory.ShoppingFragmentFactory
 import com.androiddevs.shoppinglisttestingyt.ui.viewmodel.ShoppingViewModel
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -23,6 +25,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -34,6 +37,9 @@ class AddShoppingItemFragmentTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
 
     @Before
     fun setUp() {
@@ -78,6 +84,23 @@ class AddShoppingItemFragmentTest {
         pressBack()
         val currentImageURL = fakeViewModel.currentImageUrl.getOrAwaitValue()
         assertThat(currentImageURL).isEmpty()
+    }
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("Name"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("12"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("5.5"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItem.getOrAwaitValue()).contains(ShoppingItem("Name", 12, 5.5f, ""))
     }
 
 //    @Test

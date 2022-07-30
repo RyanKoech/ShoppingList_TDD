406f1d477ecfd447beaa50497d6dd757161faeb2
RyanKoech <sirryankoech@gmail.com>
2022-07-30 09:53:18 +0300

    Added AddShoppingItemFragment to implementation

--- app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/fragment/factory/ShoppingFragmentFactory.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/fragment/factory/ShoppingFragmentFactory.kt
@@ -3,16 +3,20 @@ package com.androiddevs.shoppinglisttestingyt.ui.fragment.factory
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.androiddevs.shoppinglisttestingyt.adapter.ImageAdapter
+import com.androiddevs.shoppinglisttestingyt.ui.fragment.AddShoppingItemFragment
 import com.androiddevs.shoppinglisttestingyt.ui.fragment.ImagePickFragment
+import com.bumptech.glide.RequestManager
 import javax.inject.Inject
 
 class ShoppingFragmentFactory @Inject constructor(
-    private val imageAdapter: ImageAdapter
+    private val imageAdapter: ImageAdapter,
+    private val glide: RequestManager
 ) : FragmentFactory() {
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when(className){
             ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
+            AddShoppingItemFragment::class.java.name -> AddShoppingItemFragment(glide)
             else -> super.instantiate(classLoader, className)
         }
     }

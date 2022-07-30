32455b3a6adc62ff9320e6440021ce4379c5de09
RyanKoech <sirryankoech@gmail.com>
2022-07-30 09:18:02 +0300

    Defined tests for recyclerview item click

--- app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ui/fragment/ImagePickFragmentTest.kt
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ui/fragment/ImagePickFragmentTest.kt
@@ -1,5 +1,72 @@
 package com.androiddevs.shoppinglisttestingyt.ui.fragment
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.fragment.app.FragmentFactory
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.androiddevs.shoppinglisttestingyt.R
+import com.androiddevs.shoppinglisttestingyt.adapter.ImageAdapter
+import com.androiddevs.shoppinglisttestingyt.getOrAwaitValue
+import com.androiddevs.shoppinglisttestingyt.launchFragmentInHiltContainer
+import com.androiddevs.shoppinglisttestingyt.repository.FakeShoppingRepositoryAndroidTest
+import com.androiddevs.shoppinglisttestingyt.ui.fragment.factory.ShoppingFragmentFactory
+import com.androiddevs.shoppinglisttestingyt.ui.viewmodel.ShoppingViewModel
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import javax.inject.Inject
 
-class ImagePickFragmentTest
+@HiltAndroidTest
+@MediumTest
+@ExperimentalCoroutinesApi
+class ImagePickFragmentTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun clickImage_popBackStackAndSetImageUrl() {
+        val navController = mock(NavController::class.java)
+        val imageUrl = "test"
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<ImagePickFragment>(fragmentFactory = fragmentFactory) {
+            Navigation.setViewNavController(requireView(), navController)
+            imageAdapter.images = listOf(imageUrl)
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.rvImages)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ImageAdapter.ImageViewHolder>(
+                0,
+                click()
+            )
+        )
+
+        verify(navController).popBackStack()
+        assertThat(testViewModel.currentImageUrl.getOrAwaitValue()).isEqualTo(imageUrl)
+
+    }
+
+}

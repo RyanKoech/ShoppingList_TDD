f1eb07701e2238ceb968071fb2d38e666aadcf2e
RyanKoech <sirryankoech@gmail.com>
2022-08-01 12:45:55 +0300

    Updated to include shoppingfragment

--- app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/fragment/factory/ShoppingFragmentFactory.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/fragment/factory/ShoppingFragmentFactory.kt
@@ -3,13 +3,17 @@ package com.androiddevs.shoppinglisttestingyt.ui.fragment.factory
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.androiddevs.shoppinglisttestingyt.adapter.ImageAdapter
+import com.androiddevs.shoppinglisttestingyt.adapters.ShoppingItemAdapter
 import com.androiddevs.shoppinglisttestingyt.ui.fragment.AddShoppingItemFragment
 import com.androiddevs.shoppinglisttestingyt.ui.fragment.ImagePickFragment
+import com.androiddevs.shoppinglisttestingyt.ui.fragment.ShoppingFragment
+import com.androiddevs.shoppinglisttestingyt.ui.viewmodel.ShoppingViewModel
 import com.bumptech.glide.RequestManager
 import javax.inject.Inject
 
 class ShoppingFragmentFactory @Inject constructor(
     private val imageAdapter: ImageAdapter,
+    private val shoppingItemAdapter: ShoppingItemAdapter,
     private val glide: RequestManager
 ) : FragmentFactory() {
 
@@ -17,6 +21,9 @@ class ShoppingFragmentFactory @Inject constructor(
         return when(className){
             ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
             AddShoppingItemFragment::class.java.name -> AddShoppingItemFragment(glide)
+            ShoppingFragment::class.java.name -> ShoppingFragment(
+                shoppingItemAdapter
+            )
             else -> super.instantiate(classLoader, className)
         }
     }
